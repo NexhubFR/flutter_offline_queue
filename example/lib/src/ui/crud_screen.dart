be4fea287b838e993c82e5eead05e626973bb2c1
valmnt <valentinmont8@gmail.com>
2024-04-08 16:16:06 +0200

    [Example] Add get, patch and put

--- example/lib/src/ui/crud_screen.dart
+++ example/lib/src/ui/crud_screen.dart
@@ -8,16 +8,55 @@ import 'package:otter/network/http_method.dart';
 import 'package:otter/database/database_store.dart';
 
 class CRUDScreen extends StatelessWidget {
-  const CRUDScreen({super.key});
+  final store = OTDBStore();
+
+  CRUDScreen({super.key});
+
+  void get() {
+    final task = ExampleTask(Uri.https('dummyjson.com', '/products/1'),
+        HTTPMethod.get, {'Content-Type': 'application/json'}, {});
+
+    store.addOneTask(
+      task,
+      didFail: (task, error, stackTrace) => {log('$task, $error, $stackTrace')},
+      networkAvailable: () => {log('Network is available')},
+    );
+  }
 
   void post() {
-    final store = OTDBStore();
-
     final task = ExampleTask(
         Uri.https('dummyjson.com', '/products/add'),
         HTTPMethod.post,
         {'Content-Type': 'application/json'},
         {'title': 'BMW Pencil'});
+
+    store.addOneTask(
+      task,
+      didFail: (task, error, stackTrace) => {log('$task, $error, $stackTrace')},
+      networkAvailable: () => {log('Network is available')},
+    );
+  }
+
+  void patch() {
+    final task = ExampleTask(
+        Uri.https('dummyjson.com', '/products/1'),
+        HTTPMethod.patch,
+        {'Content-Type': 'application/json'},
+        {'title': 'iPhone Galaxy +1'});
+
+    store.addOneTask(
+      task,
+      didFail: (task, error, stackTrace) => {log('$task, $error, $stackTrace')},
+      networkAvailable: () => {log('Network is available')},
+    );
+  }
+
+  void put() {
+    final task = ExampleTask(
+        Uri.https('dummyjson.com', '/products/1'),
+        HTTPMethod.put,
+        {'Content-Type': 'application/json'},
+        {'title': 'iPhone Galaxy +1'});
 
     store.addOneTask(
       task,
@@ -34,15 +73,22 @@ class CRUDScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Center(
-                child: TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-              onPressed: () => post(),
-              child: const Text('POST'),
-            )),
+            button('GET', () => get()),
+            button('POST', () => post()),
+            button('PATCH', () => patch()),
+            button('PUT', () => put()),
           ]),
     );
+  }
+
+  Widget button(String text, void Function() onPressed) {
+    return Center(
+        child: TextButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+      ),
+      onPressed: onPressed,
+      child: Text(text),
+    ));
   }
 }
