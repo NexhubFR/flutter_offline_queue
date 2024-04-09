1b74fcb89486bc36c621708f9aa533db329a730e
valmnt <valentinmont8@gmail.com>
2024-04-09 12:24:22 +0200

    [Comment] NetworkHelper

--- lib/network/network_helper.dart
+++ lib/network/network_helper.dart
@@ -26,6 +26,17 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
 
   OTNetworkHelper(this._handler);
 
+  /// Monitors connectivity changes to process tasks when the network is available.
+  ///
+  /// This method sets up a listener for connectivity changes using `Connectivity().onConnectivityChanged`.
+  /// Upon detecting a connectivity change, if the device is not disconnected (`ConnectivityResult.none`),
+  /// it:
+  /// - Acquires a reentrant lock to ensure synchronized access to the following operations.
+  /// - Fetches tasks from the database using `_databaseProvider.getTasks()`.
+  /// - If there are tasks, it calls `execute(tasks, _handler)` to process them.
+  ///
+  /// This method ensures tasks are only executed when network connectivity is available,
+  /// preventing unnecessary processing attempts during offline periods.
   Future<void> observe() async {
     var lock = Lock(reentrant: true);
 
@@ -42,6 +53,18 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
     });
   }
 
+  /// Executes a list of tasks using the specified handler.
+  ///
+  /// Iterates over the provided list of `OTTask` objects and executes them based
+  /// on their HTTP method (`get`, `post`, `patch`, `put`, `delete`).
+  /// After each task is executed, it calls `handler.didFinish(task)` to signal completion.
+  ///
+  /// Parameters:
+  /// - `tasks`: The list of tasks to be executed.
+  /// - `handler`: The `DefaultOTTaskHandler` used to handle task completion.
+  ///
+  /// This method ensures that each task is processed according to
+  /// its specified method and properly signals when it's finished.
   @override
   Future<void> execute(List<OTTask> tasks, DefaultOTTaskHandler handler) async {
     for (var task in tasks) {
@@ -62,6 +85,17 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
     }
   }
 
+  /// Executes a GET request for the specified task.
+  ///
+  /// Performs an HTTP GET request using the task's URI.
+  /// Upon successful completion, it invokes `handler.didSuccess` with the task and response body.
+  /// If an error occurs, `handler.didFail` is called with the task, error, and stack trace.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` object representing the task to be executed.
+  ///           Its `uri` property specifies the endpoint for the GET request.
+  /// - `handler`: The `DefaultOTTaskHandler` responsible for handling the success or failure of the task.
+  ///
   @override
   Future<void> get(OTTask task, DefaultOTTaskHandler handler) async {
     await http
@@ -71,6 +105,19 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
             (error, stackTrace) => handler.didFail(task, error, stackTrace));
   }
 
+  /// Executes a POST request for the given task.
+  ///
+  /// Sends an HTTP POST request using the task's URI, headers, and body.
+  /// On success, it calls `handler.didSuccess` with the task and the response body.
+  /// If an error occurs, `handler.didFail` is invoked with the task, the error,
+  /// and the stack trace for handling the failure.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` to be executed, containing the endpoint URI,
+  ///           request headers, and the body of the request.
+  /// - `handler`: The `DefaultOTTaskHandler` used to handle the outcome of the task,
+  ///              whether successful or failed.
+  ///
   @override
   Future<void> post(OTTask task, DefaultOTTaskHandler handler) async {
     await http
@@ -80,6 +127,21 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
             (error, stackTrace) => handler.didFail(task, error, stackTrace));
   }
 
+  /// Executes a PATCH request for the specified task.
+  ///
+  /// This method performs an HTTP PATCH request to the task's URI
+  /// with the provided headers and body.
+  /// On successful completion of the request,
+  /// it triggers `handler.didSuccess` with the task and response body.
+  /// In case of an error, it invokes `handler.didFail` with the task,
+  /// the encountered error, and the stack trace.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` detailing the request, including the URI,
+  ///           headers, and the JSON-encoded body.
+  /// - `handler`: The `DefaultOTTaskHandler` for managing the task's outcome,
+  ///              addressing both success and failure scenarios.
+  ///
   @override
   Future<void> patch(OTTask task, DefaultOTTaskHandler handler) async {
     await http
@@ -89,6 +151,19 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
             (error, stackTrace) => handler.didFail(task, error, stackTrace));
   }
 
+  /// Executes a PUT request for the given task.
+  ///
+  /// Sends an HTTP PUT request to the specified URI of the task with
+  /// the provided headers and a JSON-encoded body. Upon successful request completion,
+  /// `handler.didSuccess` is invoked with the task and the response body.
+  /// If an error occurs, `handler.didFail` is called with the task,
+  /// the error, and the stack trace.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` object containing the endpoint URI, headers, and the body for the request.
+  /// - `handler`: The `DefaultOTTaskHandler` responsible for handling
+  ///              the response to the request, managing both success and failure outcomes.
+  ///
   @override
   Future<void> put(OTTask task, DefaultOTTaskHandler handler) async {
     await http
@@ -98,6 +173,19 @@ class OTNetworkHelper implements DefaultOTNetworkHelper {
             (error, stackTrace) => handler.didFail(task, error, stackTrace));
   }
 
+  /// Executes a DELETE request for the specified task.
+  ///
+  /// This method performs an HTTP DELETE request using the task's URI.
+  /// Upon successfully completing the request,
+  /// it invokes `handler.didSuccess` with the task and the response body.
+  /// If the request fails, `handler.didFail` is called with the task,
+  /// the error, and the stack trace.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` representing the DELETE operation, including the endpoint URI.
+  /// - `handler`: The `DefaultOTTaskHandler` tasked with managing the success
+  ///              or failure of the delete operation.
+  ///
   @override
   Future<void> delete(OTTask task, DefaultOTTaskHandler handler) async {
     await http
