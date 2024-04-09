517fed7a7643e34b89399b0f8e8aaf8bfaeb7df7
valmnt <valentinmont8@gmail.com>
2024-04-09 11:41:04 +0200

    [Comment] DatabaseStore && TaskHandler

--- lib/database/database_store.dart
+++ lib/database/database_store.dart
@@ -28,6 +28,17 @@ class OTDBStore implements DefaultOTDBStore {
     _connectivity = connectivity;
   }
 
+  /// Adds a single `OTTask` to the database or queue.
+  ///
+  /// Wraps the given `task` in a list and delegates to `addMultipleTasks` for consistency in handling,
+  /// including error and network availability checks.
+  /// This approach maintains uniformity with bulk task additions while simplifying single task processing.
+  ///
+  /// Parameters:
+  /// - `task`: The task to add.
+  /// - `didFail`: Callback for handling errors during task addition.
+  /// - `networkAvailable`: Callback if network is available.
+  ///
   @override
   Future<void> addOneTask(OTTask task,
       {required Function(OTTask task, Object? error, StackTrace stackTrace)
@@ -37,6 +48,21 @@ class OTDBStore implements DefaultOTDBStore {
         didFail: didFail, networkAvailable: networkAvailable);
   }
 
+  /// Adds multiple `OTTask` objects to the database based on network availability.
+  ///
+  /// This method checks for network connectivity before deciding
+  /// on the action to take with the provided tasks:
+  /// - If no network is available (`ConnectivityResult.none`), tasks are saved to the database
+  ///   for later processing. Errors in saving are handled by the `didFail` callback.
+  /// - If the network is available, it invokes `networkAvailable`.
+  ///
+  /// Parameters:
+  /// - `tasks`: A list of tasks to be added or processed.
+  /// - `didFail`: A callback invoked if saving a task to the database fails,
+  ///              providing the task, error, and stack trace.
+  /// - `networkAvailable`: A callback that is called when network connectivity is available
+  ///                       to process tasks immediately.
+  ///
   @override
   Future<void> addMultipleTasks(List<OTTask> tasks,
       {required Function(OTTask task, Object? error, StackTrace stackTrace)
--- lib/handler/task_handler.dart
+++ lib/handler/task_handler.dart
@@ -12,14 +12,43 @@ abstract class DefaultOTTaskHandler {
 class OTTaskHandler implements DefaultOTTaskHandler {
   final _databaseProvider = OTDBProvider();
 
+  /// Handles task failure.
+  ///
+  /// This method is invoked when an error occurs during task processing.
+  /// It provides a way to handle or log errors associated with a specific `OTTask`.
+  ///
+  /// Parameters:
+  /// - `task`: The task that failed.
+  /// - `error`: The error encountered during processing, which may be `null`.
+  /// - `stackTrace`: The stack trace associated with the error, providing context for debugging.
+  ///
   @override
   void didFail(OTTask task, Object? error, StackTrace stackTrace) {}
 
+  /// Completes processing of a given task.
+  ///
+  /// Upon successfully finishing the processing of a task,
+  /// this method removes the task from the database using its UUID.
+  /// This signifies that the task no longer needs to be tracked or processed further.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` that has been processed and needs to be removed from persistence.
+  ///
   @override
   Future<void> didFinish(OTTask task) async {
     await _databaseProvider.eraseTask(task.uuid);
   }
 
+  /// Handles the successful completion of a task.
+  ///
+  /// This callback is invoked when a task is completed successfully,
+  /// allowing for any post-success logic.
+  ///
+  /// Parameters:
+  /// - `task`: The `OTTask` that succeeded.
+  /// - `response`: The success response associated with the task completion.
+  ///               This could be data returned from a network request or a confirmation message.
+  ///
   @override
   void didSuccess(OTTask task, String response) {}
 }
