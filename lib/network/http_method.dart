b01fae1a3e6c54eeec0e23683c418698aad698e7
valmnt <valentinmont8@gmail.com>
2024-04-09 11:51:58 +0200

    [Comment] HTTPMethod

--- lib/network/http_method.dart
+++ lib/network/http_method.dart
@@ -3,6 +3,20 @@ library otter;
 enum HTTPMethod { get, post, patch, put, delete }
 
 extension HTTPMethodExtension on HTTPMethod {
+  /// Converts a string to its corresponding `HTTPMethod` enum.
+  ///
+  /// This static method maps string representations of HTTP methods
+  /// to their respective `HTTPMethod` enum values.
+  /// Supported methods include 'get', 'post', 'patch', 'put', and 'delete'.
+  /// If the input string does not match any supported methods,
+  /// the method logs an exception and returns `null`.
+  ///
+  /// Parameters:
+  /// - `from`: The string representation of the HTTP method to convert.
+  ///
+  /// Returns:
+  /// - The corresponding `HTTPMethod` enum, or `null`
+  ///   if the string does not match any recognized methods.
   static HTTPMethod? fromString(String from) {
     switch (from) {
       case 'get':
